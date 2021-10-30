ad2013208f4f54e851faf3d8ddab47586793b248
Sang <thanato0912@gmail.com>
2021-10-30 22:08:10 +0900

    fix: use express listen

--- api/src/index.js
+++ api/src/index.js
@@ -30,13 +30,8 @@ app.post('/api/v1/processData', async (req, res) => {
     }
   })
 })
-
 const PORT = 4000
-const server = http.createServer(app)
-server.listen(PORT, 'localhost', err => {
-  if (err) {
-    console.error(err)
-  } else {
-    console.info('Listening on port %s.', PORT)
-  }
+
+app.listen(PORT, () => {
+  console.info('Listening on port %s.', PORT)
 })
